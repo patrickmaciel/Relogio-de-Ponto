8c87171c1577ee460a0572e7166fa42878205347
Hanrrison <40416355+Hanrrison@users.noreply.github.com>
2019-07-09 00:31:57 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -11,8 +11,7 @@
 	<meta charset="UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <!-- <link rel="stylesheet" href="index.css"> -->
-
+  <link rel="stylesheet" href="css/index.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
     <title>Relógio de Ponto</title>
@@ -20,19 +19,22 @@
   </head>
 
   <body>
-
+  
+  <br>
+  <h3>Relógio de Ponto - Anfertech</h3>
   <form method="POST" enctype="multipart/form-data">
 										
 										<label for="import-arquivo" class="txt">Importar TXT</label>
-										<input id="import-arquivo" type='file' name="imptxt" accept=".txt" >
-                    <?php
-                     $txt->importartxt();
-                    ?>
+										<input id="import-arquivo" type='file' name="imptxt" accept=".txt">
+       
+										<label for="submit-arquivo" class="txt">Confirmar XML</label>
+										<input type="submit" id="submit-arquivo" name="confirmatxt" value="Confirmar Importação"><br>			
+</form>
+<?php
 
-										<label for="submit-arquivo" class="txt">Confirmar TXT</label>
-										<input type="submit" id="submit-arquivo" name="confirmatxt" value="Confirmar Importação"><br>		
+?>
 
-							
+
 <table class="table table-bordered">
   <thead>
     <tr>
@@ -45,10 +47,14 @@
   </thead>
   <tbody>
     <tr>
-      <th scope="row"><?php $txt->ler()?></th>
+    <?php $txt->importartxt()?>
     </tr>
   </tbody>
 </table>
+
+<?php
+
+?>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
